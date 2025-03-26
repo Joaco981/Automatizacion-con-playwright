f7bcf7dd79d76a6759febd787c49a199614d8331
Joaco981 <joaquinnfloress16@gmail.com>
2025-03-26 16:53:07 -0300

    codigo completo

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -4,36 +4,36 @@ dotenv.config();
 
 
 //TEST INVALIDOS
-test.describe('Sistema alumnos, DNI: 666 Pass: demo', () => { 
-  test('Test Invalido n°1', async ({ page }) => {
+test.describe('Sistema alumnos', () => { 
+  test('Test Inválido n°1', async ({ page }) => {
     await page.goto(`${process.env.BASE_URL}/alumnosnotas/`);
 
     await expect(page).toHaveTitle(/Login Alumnos/);
 
     await page.waitForSelector('[name="ctl00$ContentPlaceHolder1$TextBox1"]');
-    await page.locator('[name="ctl00$ContentPlaceHolder1$TextBox1"]').fill('666');
-    await page.locator('[name="ctl00$ContentPlaceHolder1$Clave"]').fill('demo');
+    await page.locator('[name="ctl00$ContentPlaceHolder1$TextBox1"]').fill(process.env.USUARIO_INVALIDO1 ?? '');
+    await page.locator('[name="ctl00$ContentPlaceHolder1$Clave"]').fill(process.env.PASSWORD_INVALIDO1 ?? '');
     await page.locator('[name="ctl00$ContentPlaceHolder1$ImageButton1"]').click();
     await expect(page.locator('[id="ctl00_ContentPlaceHolder1_Label2"]')).toHaveText('La combinación de usuario y clave no coincide');
   });
 });
 
-  test.describe('Sistema alumnos, DNI: abc Pass: demo', () => { 
-    test('Test invalido n°2', async ({ page }) => {
+  test.describe('Sistema alumnos', () => { 
+    test('Test inválido n°2', async ({ page }) => {
       await page.goto(`${process.env.BASE_URL}/alumnosnotas/`);
 
       await expect(page).toHaveTitle(/Login Alumnos/);
 
       await page.waitForSelector('[name="ctl00$ContentPlaceHolder1$TextBox1"]');
-      await page.locator('[name="ctl00$ContentPlaceHolder1$TextBox1"]').fill('abc');
-      await page.locator('[name="ctl00$ContentPlaceHolder1$Clave"]').fill('demo');
+      await page.locator('[name="ctl00$ContentPlaceHolder1$TextBox1"]').fill(process.env.USUARIO_INVALIDO2 ?? '');
+      await page.locator('[name="ctl00$ContentPlaceHolder1$Clave"]').fill(process.env.PASSWORD_INVALIDO2 ?? '');
       await page.locator('[name="ctl00$ContentPlaceHolder1$ImageButton1"]').click();
       await expect(page.locator('[id="ctl00_ContentPlaceHolder1_Label2"]')).toHaveText('La combinación de usuario y clave no coincide o no tiene permisos');
     });
   });
 
 //TEST VALIDOS
-test.describe('Sistema alumnos, DNI:  Pass: ', () => { 
+test.describe('Sistema alumnos', () => { 
   test('Test válido', async ({ page }) => {
     await page.goto(`${process.env.BASE_URL}/alumnosnotas/`);
 
